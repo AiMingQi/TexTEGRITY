a80f9791c0b2d6290a9f93fd09953fc0df7e520b
AiMingQi <cafevalentine@gmail.com>
2020-02-12 18:21:33 -0700

    book cover upload logic

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -2,19 +2,44 @@
     v-container
         v-card
             v-card-title Author Upload
-            v-card
+            v-card.pa-2
+                p {{now}}
                 v-file-input(
                     show-size 
-                    label="File input"
-                    v-model="file"
+                    label="Book Cover"
+                    accept=".png,.jpg"
+                    v-model="bookCoverImg"
                     )
                 div.mx-auto
-                    v-btn.mx-3(@click="uploadFile") Create Buffer
-                    v-btn.mx-3(@click="encryptFile") Encrypt File
-                    v-btn.mx-3(@click="ipfsUpload") Send to IPFS
-                    p {{fileHash}}
-                    p {{fileHashUrl}}
-                    a(:href="fileHashUrl" download target="_blank") Link
+                    v-btn.mx-3(@click="uploadBookCover") Book Cover Upload
+                    v-btn.mx-3(@click="bookImgUploadIpfs") Send to IPFS
+                    p IPFS Hash: {{bookImgHash}}
+                    v-img(:src="bookImgHashUrl" v-if="bookImgHashUrl")
+                    a(:href="bookImgHashUrl" download target="_blank") Link
+                v-text-field(
+                    v-model="bookTitle"
+                    label="Title"
+                    )
+                v-textarea(
+                    v-model="bookDescription"
+                    label="Description"
+                )
+                v-text-field(
+                    v-model="bookKeywords"
+                    label="Keywords"
+                    )
+                //- v-file-input(
+                //-     show-size 
+                //-     label="File input"
+                //-     v-model="file"
+                //-     )v-btn.mx-3(@click="uploadFile") Create Buffer
+                //- div.mx-auto
+                //-     v-btn.mx-3(@click="uploadFile") Create Buffer
+                //-     v-btn.mx-3(@click="encryptFile") Encrypt File
+                //-     v-btn.mx-3(@click="ipfsUpload") Send to IPFS
+                //-     p {{fileHash}}
+                //-     p {{fileHashUrl}}
+                //-     a(:href="fileHashUrl" download target="_blank") Link
                     
 
 </template>
@@ -28,7 +53,17 @@ export default {
             fileBuffer: null,
             fileHash: null,
             fileHashUrl: null,
-
+            bookTitle: 'Untitled',
+            bookCoverImg: null,
+            bookImgHash: null,
+            bookImgHashUrl: null,
+            bookDescription: 'Needs Descriptions',
+            bookKeywords: "#Pegabuffacorn",
+            bookListing: {
+                img: this.bookImgHashUrl,
+                title: this.bookTitle,
+                description: this.bookDescription
+            }
         }
     },
     methods: {
@@ -36,32 +71,61 @@ export default {
             this.selectedFile = event
             
         },
-        uploadFile () {
+        // uploadFile () {
+        //     const reader = new FileReader;
+        //     reader.readAsArrayBuffer(this.file);
+        //     console.log("Buffering...", this.file)
+        //     reader.onloadend = () => {
+        //         this.fileBuffer = Buffer(reader.result)
+        //         // var arrayBuffer = reader.result;
+        //         // this.fileBuffer = new Uint8Array(arrayBuffer);
+        //     console.log("Buffer: ", this.fileBuffer);
+        //     }
+        // },
+        uploadBookCover () {
             const reader = new FileReader;
-            reader.readAsArrayBuffer(this.file);
-            console.log("Buffering...", this.file)
+            reader.readAsArrayBuffer(this.bookCoverImg);
+            console.log("Buffering...", this.bookCoverImg)
             reader.onloadend = () => {
-                this.fileBuffer = Buffer(reader.result)
+                this.bookImgBuffer = Buffer(reader.result)
                 // var arrayBuffer = reader.result;
                 // this.fileBuffer = new Uint8Array(arrayBuffer);
-            console.log("Buffer: ", this.fileBuffer);
+            console.log("Buffer: ", this.bookImgBuffer);
             }
+        },
+        createJson () {
+            // const data = JSON.stringify(this.arr)
         },
         encryptFile () {
             console.log('Encrypting...')
+        },
+        async bookImgUploadIpfs () {
+            console.log("uploading Book Cover to IPFS....")
+            ipfs.add(this.bookImgBuffer, (error, result) => {
+                if (error || !result) {
+                    console.log("Error!")
+                    return;
+                }
+                else {
+                    this.bookImgHash = result[0].hash
+                    this.bookImgHashUrl = "http://ipfs.io/ipfs/" + this.bookImgHash
+                    console.log("Book Cover Loaded to IPFS")
+                    return;
+                }
+            })
         },
         async ipfsUpload () {
             console.log("Uploading...");
             ipfs.add(this.fileBuffer, (error, result) => {
                 if (error || !result) {
-                console.log("Error!")
-                return;
+                    console.log("Error!")
+                    return;
                 }
                 else {
-                this.fileHash = result[0].hash
-                this.fileHashUrl = "http://ipfs.io/ipfs/" + this.fileHash
-                console.log("Success!")
-                return;
+                    this.fileHash = result[0].hash
+                    this.fileHashUrl = "http://ipfs.io/ipfs/" + this.fileHash
+                    console.log("Success!")
+                    return;
                 }
             });
         }
@@ -82,6 +146,16 @@ export default {
             //     }
             // })
         
+    },
+    computed: {
+        bookListingJSON: function () {
+
+            const listingJson = JSON.stringify(this.arr)
+            return listingJson
+        },
+        now: function () {
+            return Date.now()
+        } 
     }
 }
 </script>
