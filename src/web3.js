c1f34bf20ba95ee3a664034f385d294828855e0d
AiMingQi <cafevalentine@gmail.com>
2020-02-15 14:19:25 -0700

    maybe fix of web3

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import Web3 from 'web3';
 
-import Portis from '@portis/web3';
+// import Portis from '@portis/web3';
 
 // const portis = new Portis('8f7a2d9f-af0e-42f5-889c-f1542aab0118', 'mainnet');
 // const web3 = new Web3(portis.provider);
@@ -18,9 +18,9 @@ if (window.ethereum) {
   // Legacy dapp browsers...
   window.web3 = new Web3(web3.currentProvider);
 } else {
-    const portis = new Portis('8f7a2d9f-af0e-42f5-889c-f1542aab0118', 'rinkeby');
-    window.web3 = new Web3(portis.provider);
-    // const rpcURL = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/cfb3b8a701bb4f389604f99cb506097d')
+    // const portis = new Portis('8f7a2d9f-af0e-42f5-889c-f1542aab0118', 'rinkeby');
+    // window.web3 = new Web3(portis.provider);
+    // // const rpcURL = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/cfb3b8a701bb4f389604f99cb506097d')
     // window.web3 = new Web3(rpcURL)
   // Non-dapp browsers...
   console.log(
